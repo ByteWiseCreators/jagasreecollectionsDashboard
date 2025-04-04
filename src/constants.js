5ece09cf0e3a7fde31940bc98dd67d306af729a5
hariharan-dev-05 <hariharandeveloper05@gmail.com>
2025-04-04 19:58:22 +0530

    Server URL Updated for testing purpose

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-// export const resourceURL = "https://appsail-50025384581.development.catalystappsail.in";
-export const resourceURL = "http://localhost:8000";
+export const resourceURL = "https://api-g4fh4sabra-uc.a.run.app";
+// export const resourceURL = "http://localhost:3001";
