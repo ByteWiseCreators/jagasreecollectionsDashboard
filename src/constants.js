ff84a07a459d1f4e28f7bf2c82c7587f843b26f2
hariharan-dev-05 <hariharandeveloper05@gmail.com>
2025-04-05 21:42:19 +0530

    Server Updated

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const resourceURL = "https://api-g4fh4sabra-uc.a.run.app";
+export const resourceURL = "https://jagasreecollectionsserver.onrender.com";
 // export const resourceURL = "http://localhost:3001";
